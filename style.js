be8a4eb13966ab0917a0feaea8d0b9b71d0281af
Dumiiiii <godoroja.dum@gmail.com>
2024-03-06 09:08:59 +0100

    Yathzee

---
+++ style.js
@@ -0,0 +1,29 @@
+
+// dubblesteen rollen en Maxrollens
+
+const diceThrow = [1, 2, 3, 4, 5];
+let canThrow = [true, true, true, true, true];
+let Rollmade = 0;
+const Maxroll = 3
+
+function rollAllDice(){
+if (Rollmade < Maxroll)
+  for (let i = 0; i < diceThrow.length; i++) {
+    if (canThrow[i]) {
+      const rollResult = Math.floor(Math.random() * 6) + 1;
+      diceThrow[i] = rollResult;
+      console.log("Dice " + (i + 1) + " rolled: " + rollResult);
+      document.getElementById("die" + (i + 1)).textContent = rollResult;
+    }
+  }
+  Rollmade++;
+}
+//Rolling Updater
+
+// Reset
+function resetButton(){
+document.getElementById("resetButton").addEventListener("click", function() {
+console.log("Reset")
+});
+resetButton();
+}
